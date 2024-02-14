768c581ae59e9ee1b71d795889c85bc171949b3d
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2024-02-14 16:57:39 +0700

    add test empty array when request api fails

--- tests/Feature/CapresTest.php
+++ tests/Feature/CapresTest.php
@@ -2,11 +2,24 @@
 
 use App\DTO\ProfilData;
 use App\Services\ApiCapresService;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
 
 it('returns an array of capres data', function () {
     $result = ApiCapresService::getCapresData();
 
     expect($result)->toBeArray();
+});
+
+it('returns an empty array when API request fails', function () {
+    Http::fake([
+        'https://mocki.io/v1/92a1f2ef-bef2-4f84-8f06-1965f0fca1a7' => Http::response([], 500)
+    ]);
+
+    $result = ApiCapresService::getCapresData();
+
+    expect($result)->toBeArray();
+    expect($result)->toBeEmpty();
 });
 
 it('returns an array of parsed Capres data', function () {
